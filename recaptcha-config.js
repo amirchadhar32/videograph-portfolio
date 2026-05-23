981406c164ef8f7c0e45cc075bdd3f0bae7cebaf
Amir <mithuchander@gmail.com>
2026-05-23 16:55:10 +0500

    add key

--- recaptcha-config.js
+++ recaptcha-config.js
@@ -10,6 +10,6 @@
  *
  * Leave empty ('') to disable reCAPTCHA and submit like a normal Netlify form.
  */
-// 6Ld5wfgsAAAAAJO4boml-dJjesZIhpdhGcqXDTeT
-window.RECAPTCHA_SITE_KEY = '';
+// 
+window.RECAPTCHA_SITE_KEY = '6Ld5wfgsAAAAAJO4boml-dJjesZIhpdhGcqXDTeT';
 window.RECAPTCHA_ACTION = 'contact_submit';
